42f5e657326b3f3341bdf1e696d5a6a8a0b7bf5b
Ben Wolpers <benwolpers@gmail.com>
2023-11-05 18:18:35 -0500

    test

---
+++ script.js
@@ -0,0 +1,23 @@
+const container = document.getElementById('book-container'); // Select the book container element
+
+fetch('/api/data')
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((book) => {
+      const bookElement = document.createElement('div');
+      bookElement.classList.add('record');
+      bookElement.innerHTML = `
+        <h2>${book.title}</h2>
+        <p>Author: ${book.author}</p>
+        <p>Genre: ${book.genre}</p>
+        <p>Published: ${book.publicationYear}</p>
+        <p>${book.description}</p>
+        <h3>Reviews:</h3>
+        <ul>
+          ${book.reviews.map((review) => `<li>${review}</li>`).join('')}
+        </ul>
+        <img src="${book.imageLink}" alt="${book.title} Cover">
+      `;
+      container.appendChild(bookElement);
+    });
+  });
--- server.js
+++ server.js
@@ -1,1 +1,30 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000; // Use the default port or 3000
 
+// Create an array of book data
+const bookData = [
+  {
+    id: 1,
+    title: 'Book 1',
+    author: 'Author 1',
+    genre: 'Fiction',
+    publicationYear: 2020,
+    description: 'Description of Book 1',
+    reviews: [
+      'Review 1',
+      'Review 2',
+      'Review 3'
+    ],
+    imageLink: 'link-to-book-cover-1.jpg',
+  },
+  // Add more book data records
+];
+
+app.get('/api/data', (req, res) => {
+  res.json(bookData); // Serve the book data as JSON when someone accesses /api/data
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
