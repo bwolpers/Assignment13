317e9753346c86d5fd9f3866066bfc3d87534d7b
Ben Wolpers <benwolpers@gmail.com>
2023-11-13 22:19:54 -0500

    assignment 14

--- public/script.js
+++ public/script.js
@@ -1,82 +1,176 @@
-const showBooks = async () => {
-  const fetchedBooks = await getBooks();
-  const container = document.getElementById('book-container');
+document.addEventListener("DOMContentLoaded", () => {
+  let selectedBook = null;
 
-  if (!fetchedBooks) {
-    console.error('Invalid JSON');
-    return;
-  }
+  const getBooks = async () => {
+      try {
+          const response = await fetch("/api/books");
+          if (!response.ok) {
+              throw new Error("Failed to fetch books");
+          }
+          return response.json();
+      } catch (error) {
+          console.error(error);
+      }
+  };
 
-  fetchedBooks.forEach((book) => {
-    const bookContainer = document.createElement('div');
-    bookContainer.classList.add('book-container'); // Add the book container class
-    container.appendChild(bookContainer);
+  const resetBooks = async () => {
+      try {
+          const response = await fetch("/api/books/", {
+              method: "DELETE",
+          });
 
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.title;
-    bookContainer.appendChild(h2);
+          if (!response.ok) {
+              throw new Error("Error resetting books");
+          }
 
-    const pAuthor = document.createElement('p');
-    pAuthor.innerHTML = `by ${book.author}`;
-    bookContainer.appendChild(pAuthor);
+          showBooks();
+      } catch (error) {
+          console.error("Error resetting books:", error);
+      }
+  };
 
-    const pGenre = document.createElement('p');
-    pGenre.innerHTML = `Genre: ${book.genre}`;
-    bookContainer.appendChild(pGenre);
+  const getReviews = () => {
+      const inputs = document.querySelectorAll("#review-boxes input");
+      return Array.from(inputs).map((input) => input.value);
+  };
 
-    const pPublished = document.createElement('p');
-    pPublished.innerHTML = `Published: ${book.publicationYear}`;
-    bookContainer.appendChild(pPublished);
+  const showBooks = async () => {
+      try {
+          const books = await getBooks();
+          const booksDiv = document.getElementById("book-list");
+          booksDiv.innerHTML = "";
 
-    const pDescription = document.createElement('p');
-    pDescription.innerHTML = book.description;
-    bookContainer.appendChild(pDescription);
+          books.forEach((book) => {
+              const section = document.createElement("section");
+              section.classList.add("book");
+              booksDiv.appendChild(section);
 
-    const h3Reviews = document.createElement('h3');
-    h3Reviews.innerHTML = 'Reviews';
-    bookContainer.appendChild(h3Reviews);
+              const a = document.createElement("a");
+              a.href = "#";
+              section.appendChild(a);
 
-    const ulReviews = document.createElement('ul');
-    bookContainer.appendChild(ulReviews);
+              const h3 = document.createElement("h3");
+              h3.innerHTML = book.title;
+              a.appendChild(h3);
 
-    book.reviews.forEach((review) => {
-      const liReview = document.createElement('li');
-      liReview.innerHTML = review;
-      ulReviews.appendChild(liReview);
-    });
+              a.onclick = (e) => {
+                  e.preventDefault();
+                  if (selectedBook) {
+                      selectedBook.h3.style.opacity = 0.5;
+                  }
 
-    const img = document.createElement('img');
-    img.src = `${book.imageLink}`;
-    img.alt = `${book.title} Cover`;
-    bookContainer.appendChild(img);
-  });
-};
+                  const isSelected = a.classList.toggle("selected");
 
+                  if (isSelected) {
+                      selectedBook = { a, h3 };
+                  } else {
+                      selectedBook = null;
+                  }
 
+                  displayDetailsBook(book);
+              };
+          });
+      } catch (error) {
+          console.error("Error fetching books:", error);
+      }
+  };
 
+  const displayDetailsBook = (book) => {
+      const bookDetails = document.getElementById("book-details");
+      bookDetails.innerHTML = "";
 
-const getBooks = async () => {
-  try {
-    const response = await fetch('/api/data');
-    const data = await response.json();
+      const h3 = document.createElement("h3");
+      h3.innerHTML = book.title;
+      bookDetails.appendChild(h3);
 
-    const books = data.map((bookData) => {
-      return {
-        title: bookData.title,
-        author: bookData.author,
-        genre: bookData.genre,
-        publicationYear: bookData.publicationYear,
-        description: bookData.description,
-        reviews: bookData.reviews,
-        imageLink: bookData.imageLink,
-      };
-    });
+      const pAuthor = document.createElement("p");
+      pAuthor.innerHTML = `Author: ${book.author}`;
+      bookDetails.appendChild(pAuthor);
 
-    return books;
-  } catch (error) {
-    console.error('Error:', error);
-    return [];
-  }
-};
+      const pGenre = document.createElement("p");
+      pGenre.innerHTML = `Genre: ${book.genre}`;
+      bookDetails.appendChild(pGenre);
 
-window.onload = () => showBooks();
+      const pPublished = document.createElement("p");
+      pPublished.innerHTML = `Published: ${book.publicationYear}`;
+      bookDetails.appendChild(pPublished);
+
+      const p = document.createElement("p");
+      p.innerHTML = book.description;
+      bookDetails.appendChild(p);
+
+      const h3Reviews = document.createElement("h3");
+      h3Reviews.innerHTML = "Reviews";
+      bookDetails.appendChild(h3Reviews);
+
+      const ulReviews = document.createElement("ul");
+      bookDetails.appendChild(ulReviews);
+
+      book.reviews.forEach((review) => {
+          const li = document.createElement("li");
+          li.innerHTML = review;
+          ulReviews.appendChild(li);
+      });
+  };
+
+  const addBook = async (e) => {
+      e.preventDefault();
+      const form = document.getElementById("add-book-form");
+      const formData = new FormData(form);
+
+      const publicationYear = formData.get("publicationYear");
+      if (isNaN(publicationYear)) {
+          console.log("Publication Year must be a number");
+          return;
+      }
+
+      if (form._id.value == -1) {
+          formData.delete("_id");
+          formData.append("reviews", getReviews());
+
+          try {
+              const response = await fetch("/api/books", {
+                  method: "POST",
+                  body: formData,
+              });
+
+              if (!response.ok) {
+                  console.log("Error posting data");
+                  return;
+              }
+
+              const responseData = await response.json();
+              resetForm();
+              document.querySelector(".dialog").classList.add("transparent");
+              showBooks();
+          } catch (error) {
+              console.error("Error posting data:", error);
+          }
+      }
+  };
+
+  const resetForm = () => {
+      const form = document.getElementById("add-book-form");
+      form.reset();
+      form._id = "-1";
+      document.getElementById("review-boxes").innerHTML = "";
+  };
+
+  const showHideAdd = (e) => {
+      e.preventDefault();
+      document.querySelector(".dialog").classList.remove("transparent");
+      resetForm();
+  };
+
+  const addReview = (e) => {
+      e.preventDefault();
+      const section = document.getElementById("review-boxes");
+      const input = document.createElement("input");
+      input.type = "text";
+      section.appendChild(input);
+  };
+
+  showBooks();
+  document.getElementById("add-book-form").addEventListener("submit", addBook);
+  document.getElementById("add-review").addEventListener("click", addReview);
+});
--- server.js
+++ server.js
@@ -1,64 +1,93 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const Joi = require("joi");
+const multer = require("multer");
+app.use(express.static("public"));
+app.use(express.json());
+const cors = require("cors");
+app.use(cors());
 
-app.use(express.static('public'));
+const upload = multer({ dest: __dirname + "/public/images" });
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
 });
 
-app.get('/api/data', (req, res) => {
-    const books = [
-        {
-            title: 'The Secret',
-            author: 'Jack Reacher',
-            genre: 'Thriller',
-            publicationYear: 2020,
-            description: 'In this gripping thriller, Jack Reacher embarks on a relentless quest to uncover "The Secret." As he delves deeper into the mysterious conspiracy, he discovers a truth that could change everything. A pulse-pounding page-turner that will keep you on the edge of your seat from start to finish.',
-            reviews: ['An electrifying read! Jack Reacher at his best.', 'I couldn\'t put this book down. The suspense is off the charts!', 'A must-read for thriller fans.'],
-            imageLink: 'images/bookCover.jpg',
-        },
-        {
-            title: 'The Exchange: After the Firm',
-            author: 'John Grisham',
-            genre: 'Legal Thriller',
-            publicationYear: 2020,
-            description: 'In this legal thriller, John Grisham takes us back to the world of lawyers and courtroom drama. "The Exchange" is a gripping tale of a young attorney, fresh out of law school, who stumbles upon a dark secret within his prestigious law firm. As he unravels the truth, he faces a dangerous choice that could change his life forever.',
-            reviews: ['Classic John Grisham. A legal thriller with heart.', 'Riveting and thought-provoking. A page-turner that makes you question ethics and justice.', 'A must-read for fans of legal thrillers.'],
-            imageLink: 'images/bookCover2.jpg',
-        },
-        {
-            title: 'King of Greed',
-            author: 'Ana Huang',
-            genre: 'Contemporary Romance',
-            publicationYear: 2020,
-            description: '"King of Greed" is a heartwarming contemporary romance that follows the journey of two individuals brought together by fate. Amidst the bustling streets of New York City, they find love, hope, and a second chance at happiness. Ana Huang\'s storytelling shines in this tale of love, redemption, and the pursuit of dreams.',
-            reviews: ['A beautiful love story that tugs at your heartstrings.', 'Ana Huang\'s writing is simply enchanting. I couldn\'t help but fall in love with the characters.', 'A delightful read that left me with a smile on my face.'],
-            imageLink: 'images/bookCover3.jpg',
-        },
-        {
-            title: 'Fourth Wing',
-            author: 'Rebecca Yarros',
-            genre: 'Science Fiction',
-            publicationYear: 2021,
-            description: 'In Fourth Wing, Rebecca Yarros takes us on a thrilling journey through a dystopian future where the fate of humanity hangs in the balance. With high-stakes action, complex characters, and a plot full of unexpected twists, this science fiction masterpiece will leave you on the edge of your seat.',
-            reviews: ['A mind-bending journey into the future. Yarros delivers a gripping story!', 'An instant classic. I couldn\'t stop reading until the very end.', 'Science fiction fans, don\'t miss this one.'],
-            imageLink: 'images/bookCover4.jpg',
-        },
-        {
-            title: 'Lessons in Chemistry',
-            author: 'Bonnie Garmus',
-            genre: 'Historical Fiction',
-            publicationYear: 2022,
-            description: 'Lessons in Chemistry is a beautifully written historical fiction novel that transports readers to the mid-20th century. With a brilliant scientist as the protagonist and a tale of resilience and empowerment, Bonnie Garmus crafts a story that leaves a lasting impact.',
-            reviews: ['A masterpiece of historical fiction. Garmus weaves a captivating narrative.', 'I was captivated by the protagonist\'s journey. A must-read for fans of the genre.', 'Historical fiction at its finest.'],
-            imageLink: 'images/bookCover5.jpg',
-        },
-    ];
+let books = [
+    {
+        _id: 1,
+        title: "The Great Gatsby",
+        author: "F. Scott Fitzgerald",
+        genre: "Classic",
+        publicationYear: 1925,
+        description: "A novel about the American Dream",
+        reviews: ["Captivating!", "A must-read"]
+    },
+    {
+        _id: 2,
+        title: "To Kill a Mockingbird",
+        author: "Harper Lee",
+        genre: "Fiction",
+        publicationYear: 1960,
+        description: "A story of racial injustice in the American South",
+        reviews: ["Powerful!", "Touches the heart"]
+    },
+    {
+        _id: 3,
+        title: "1984",
+        author: "George Orwell",
+        genre: "Dystopian",
+        publicationYear: 1949,
+        description: "A chilling vision of a totalitarian future",
+        reviews: ["Thought-provoking", "Disturbingly accurate"]
+    },
+];
 
-    res.json(books);
+app.get("/api/books", (req, res) => {
+    res.send(books);
 });
+
+app.post("/api/books", upload.single("img"), async (req, res) => {
+    try {
+        const result = validateBookInput(req.body);
+
+        if (result.error) {
+            res.status(400).send(result.error.details[0].message);
+            return;
+        }
+
+        const book = {
+            _id: books.length + 1,
+            title: req.body.title,
+            author: req.body.author,
+            genre: req.body.genre,
+            publicationYear: req.body.publicationYear,
+            description: req.body.description,
+            reviews: req.body.reviews.split(",")
+        };
+
+        books.push(book);
+        res.send(books);
+    } catch (error) {
+        console.error("Error processing request:", error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+const validateBookInput = (book) => {
+    const schema = Joi.object({
+        _id: Joi.allow(""),
+        title: Joi.string().min(3).required(),
+        author: Joi.string().min(3).required(),
+        genre: Joi.string().min(3).required(),
+        publicationYear: Joi.number().min(1900).required(),
+        description: Joi.string().min(3).required(),
+        reviews: Joi.allow("")
+    });
+
+    return schema.validate(book);
+};
 
 app.listen(3000, () => {
-    console.log("listening");
+    console.log("Server is listening on port 3000");
 });
